527afd9e273ab84b748c8a0f45c2f9a6ebf1b76b
lifegpc <g1710431395@gmail.com>
2022-05-05 14:02:51 +0800

    Add more information to SMTCSM

---
+++ SMTCSM/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System Media Transport Controls Sessions Manager Server")]
+[assembly: AssemblyDescription("System Media Transport Controls Sessions Manager Server")]
+[assembly: AssemblyCompany("lifegpc")]
+[assembly: AssemblyProduct("SMTCSM")]
+[assembly: AssemblyCopyright("Copyright (C) 2021-2022  lifegpc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
